09b9ba61b3757297f140ddb0cca639c414a40028
nick <nick@htmlguys.com>
2016-02-11 13:00:08 +0800

    Updated javascript structure. Removed the private functions.

--- static/assets/js/specific/tooltipster/tooltipster.js
+++ static/assets/js/specific/tooltipster/tooltipster.js
@@ -1,25 +1,25 @@
+function attachtooltipster( $container ) {
+
+	$( ".js-show-tooltip", $container ).each(function() {
+		var $me = $( this ),
+			tooltip_position = ( $me.data( "position" ) === undefined ) ? "top" : $me.data( "position" );
+		$me.tooltipster( { position: tooltip_position } ).click( function( e ) {
+			e.preventDefault();
+		} );
+	} );
+
+	if ( helper.viewport().width <= helper.mediaWidth.xs ) {
+		$( ".js-show-tooltip", $container ).tooltipster( 'option', 'position', 'top' );
+	}
+
+}
+
 ( function( $ ) {
 
 	$( document ).ready( function() {
 
-		FRAMEWORK.attachtooltipster = function( $container ) {
-
-			$( ".js-show-tooltip", $container ).each(function() {
-				var $me = $( this ),
-					tooltip_position = ( $me.data( "position" ) === undefined ) ? "top" : $me.data( "position" );
-				$me.tooltipster( { position: tooltip_position } ).click( function( e ) {
-					e.preventDefault();
-				});
-			});
-
-			if ( FRAMEWORK.fn.viewport().width <= FRAMEWORK.mediaWidth.xs ) {
-				$( ".js-show-tooltip", $container ).tooltipster( 'option', 'position', 'top' );
-			}
-
-		}
-
-		FRAMEWORK.attachtooltipster( $("body") );
+		attachtooltipster( $("body") );
 
 	} );
 
-})( jQuery );
+} )( jQuery );
